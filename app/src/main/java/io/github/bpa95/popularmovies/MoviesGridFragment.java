141671d517a3cc27ca33dd60b8f5f13c3c34e510
bpa95 <berpaval@gmail.com>
2016-07-13 02:37:14 +0300

    feat: Fetch raw json string from TheMovieDatabase

--- app/src/main/java/io/github/bpa95/popularmovies/MoviesGridFragment.java
+++ app/src/main/java/io/github/bpa95/popularmovies/MoviesGridFragment.java
@@ -1,13 +1,22 @@
 package io.github.bpa95.popularmovies;
 
 import android.app.Fragment;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 
 /**
@@ -39,6 +48,83 @@ public class MoviesGridFragment extends Fragment {
         GridView listView = (GridView) rootView.findViewById(R.id.movies_grid_view);
         listView.setAdapter(mMovieAdapter);
 
+        new FetchMoviesTask().execute();
+
         return rootView;
+    }
+
+    private class FetchMoviesTask extends AsyncTask<String, Void, String> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string
+            String moviesJsonStr = null;
+
+            try {
+                // Construct the URL for the Movie Database query
+                final String MOVIES_BASE_URL = "http://api.themoviedb.org/3";
+                final String POPULAR_PATH = "movie/popular";
+                final String TOP_RATED_PATH = "movie/top_rated";
+                final String API_KEY_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                        .appendEncodedPath(POPULAR_PATH)
+                        .appendQueryParameter(API_KEY_PARAM, BuildConfig.MOVIE_DATABASE_API_KEY)
+                        .build();
+
+                Log.v(LOG_TAG, builtUri.toString());
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to TheMovieDatabase, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuilder buffer = new StringBuilder();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line).append('\n');
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return moviesJsonStr;
+        }
     }
 }
