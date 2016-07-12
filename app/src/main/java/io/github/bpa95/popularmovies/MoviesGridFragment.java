311d96c81a2c918155847b7c44fc4f0ee1cf9510
bpa95 <berpaval@gmail.com>
2016-07-12 19:19:45 +0300

    feat: create layout for main screen

---
+++ app/src/main/java/io/github/bpa95/popularmovies/MoviesGridFragment.java
@@ -0,0 +1,18 @@
+package io.github.bpa95.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoviesGridFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_movies_grid, container, false);
+        return rootView;
+    }
+}
