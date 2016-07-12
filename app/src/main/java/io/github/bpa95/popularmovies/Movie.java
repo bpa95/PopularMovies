07c5f2e6055f0201e3fb4db265808a82637ba57f
bpa95 <berpaval@gmail.com>
2016-07-12 19:47:02 +0300

    feat: Fill movies grid with dummy data

---
+++ app/src/main/java/io/github/bpa95/popularmovies/Movie.java
@@ -0,0 +1,9 @@
+package io.github.bpa95.popularmovies;
+
+public class Movie {
+    int image;
+
+    public Movie(int image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/io/github/bpa95/popularmovies/MoviesGridFragment.java
+++ app/src/main/java/io/github/bpa95/popularmovies/MoviesGridFragment.java
@@ -6,13 +6,36 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import java.util.Arrays;
+
+/**
+ * A fragment containing the grid view of movies
+ */
 public class MoviesGridFragment extends Fragment {
+
+    private MovieAdapter mMovieAdapter;
+
+    private Movie[] movies = new Movie[]{
+            new Movie(R.drawable.interstellar),
+            new Movie(R.drawable.interstellar),
+            new Movie(R.drawable.interstellar),
+            new Movie(R.drawable.interstellar),
+            new Movie(R.drawable.interstellar)
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movies_grid, container, false);
+
+        mMovieAdapter = new MovieAdapter(getActivity(), Arrays.asList(movies));
+
+        // Get a reference to the GridView, and attach this adapter to it.
+        GridView listView = (GridView) rootView.findViewById(R.id.movies_grid_view);
+        listView.setAdapter(mMovieAdapter);
+
         return rootView;
     }
 }
