093a965503389a4e56de08c09e4d89b5b5563635
bpa95 <berpaval@gmail.com>
2016-08-30 19:32:37 +0300

    feat: Add info for Content Provider in MovieContract and test its correctness

---
+++ app/src/androidTest/java/io/github/bpa95/popularmovies/data/TestMovieContract.java
@@ -0,0 +1,42 @@
+package io.github.bpa95.popularmovies.data;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+/*
+    Students: This is NOT a complete test for the WeatherContract --- just for the functions
+    that we expect you to write.
+ */
+public class TestMovieContract extends AndroidTestCase {
+
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    private static final String TEST_SORT_BY = "/popular";
+
+    public void testBuildMovieSortedUri() {
+        Uri uri = MoviesContract.MovieEntry.buildMovieSortedUri(TEST_SORT_BY);
+        assertNotNull("Error: Null Uri returned.", uri);
+        assertEquals("Error: SortBy not properly appended to the end of the Uri",
+                TEST_SORT_BY, uri.getLastPathSegment());
+        assertEquals("Error: Uri doesn't match expected result",
+                uri.toString(),
+                "content://io.github.bpa95.popularmovies/movie/%2Fpopular");
+        assertEquals("Error: getSortByFromUri doesn't work",
+                MoviesContract.MovieEntry.getSortByFromUri(uri), TEST_SORT_BY);
+    }
+
+    private static final int TEST_MOVIE_ID = 123;
+
+    public void testBuildTrailersByMovieUri() {
+        Uri uri = MoviesContract.TrailerEntry.buildTrailersByMovieUri(TEST_MOVIE_ID);
+        assertNotNull("Error: Null Uri returned.", uri);
+        assertEquals("Error: Movie id not properly appended to the end of the Uri",
+                TEST_MOVIE_ID, Integer.parseInt(uri.getLastPathSegment()));
+        assertEquals("Error: Uri doesn't match expected result",
+                uri.toString(),
+                "content://io.github.bpa95.popularmovies/trailer/123");
+        assertEquals("Error: getMovieIdFromUri doesn't work",
+                MoviesContract.TrailerEntry.getMovieIdFromUri(uri), TEST_MOVIE_ID);
+    }
+
+
+}
--- app/src/main/java/io/github/bpa95/popularmovies/data/MoviesContract.java
+++ app/src/main/java/io/github/bpa95/popularmovies/data/MoviesContract.java
@@ -1,5 +1,8 @@
 package io.github.bpa95.popularmovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +10,24 @@ import android.provider.BaseColumns;
  */
 public class MoviesContract {
 
+    public static final String CONTENT_AUTHORITY = "io.github.bpa95.popularmovies";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = "movie";
+    public static final String PATH_TRAILER = "trailer";
+
+
     public static final class MovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
         public static final String TABLE_NAME = "movie";
 
         /** Movie id as returned by API */
@@ -22,14 +42,46 @@ public class MoviesContract {
         public static final String COLUMN_VOTE_AVERAGE = "vote_average";
         /** Synopsis of the movie */
         public static final String COLUMN_OVERVIEW = "overview";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildMovieSortedUri(String sortBy) {
+            return CONTENT_URI.buildUpon().appendPath(sortBy).build();
+        }
+
+        public static String getSortByFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
     }
 
     public static final class TrailerEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRAILER).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+
         public static final String TABLE_NAME = "trailer";
 
         /** Foreign key into the movie table */
         public static final String COLUMN_MOVIE_ID = "movie_id";
         /** Url to the trailer */
         public static final String COLUMN_TRAILER_PATH = "trailer_path";
+
+        public static Uri buildTrailerUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildTrailersByMovieUri(int movieId) {
+            return CONTENT_URI.buildUpon().appendPath(Integer.toString(movieId)).build();
+        }
+
+        public static int getMovieIdFromUri(Uri uri) {
+            return Integer.parseInt(uri.getPathSegments().get(1));
+        }
     }
 }
