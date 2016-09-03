f04e8f5dc6512660cfaf6a626f816288daf0f071
bpa95 <berpaval@gmail.com>
2016-09-03 16:18:35 +0300

    feat: Update Movie class to match db

--- app/src/main/java/io/github/bpa95/popularmovies/Movie.java
+++ app/src/main/java/io/github/bpa95/popularmovies/Movie.java
@@ -11,24 +11,29 @@ public class Movie implements Parcelable {
     int id;
     Uri posterPath;
     String title;
-    String releaseDate;
+    long releaseDate;
+    double popularity;
     double voteAverage;
     String overview;
+    boolean favorite;
 
-    public Movie(JSONObject jsonMovie) throws JSONException {
+    Movie(JSONObject jsonMovie) throws JSONException {
         // These are the names of the JSON objects that need to be extracted
         final String TMDB_MOVIE_ID = "id";
         final String TMDB_POSTER_PATH = "poster_path";
         final String TMDB_TITLE = "title";
         final String TMDB_RELEASE_DATE = "release_date";
+        final String TMDB_POPULARITY = "popularity";
         final String TMDB_VOTE_AVERAGE = "vote_average";
         final String TMDB_OVERVIEW = "overview";
 
         id = jsonMovie.getInt(TMDB_MOVIE_ID);
         title = jsonMovie.getString(TMDB_TITLE);
-        releaseDate = jsonMovie.getString(TMDB_RELEASE_DATE);
+        releaseDate = parseDate(jsonMovie.getString(TMDB_RELEASE_DATE));
+        popularity = jsonMovie.getDouble(TMDB_POPULARITY);
         voteAverage = jsonMovie.getDouble(TMDB_VOTE_AVERAGE);
         overview = jsonMovie.getString(TMDB_OVERVIEW);
+        favorite = false;
 
         final String BASE_URL = "http://image.tmdb.org/t/p/";
         final String IMAGE_SIZE = "w185";
@@ -40,13 +45,19 @@ public class Movie implements Parcelable {
                 .build();
     }
 
-    protected Movie(Parcel in) {
+    private long parseDate(String date) {
+        return 0;
+    }
+
+    private Movie(Parcel in) {
         id = in.readInt();
         posterPath = in.readParcelable(Uri.class.getClassLoader());
         title = in.readString();
-        releaseDate = in.readString();
+        releaseDate = in.readLong();
+        popularity = in.readDouble();
         voteAverage = in.readDouble();
         overview = in.readString();
+        favorite = in.readInt() == 1;
     }
 
     public static final Creator<Movie> CREATOR = new Creator<Movie>() {
@@ -71,8 +82,10 @@ public class Movie implements Parcelable {
         parcel.writeInt(id);
         parcel.writeParcelable(posterPath, i);
         parcel.writeString(title);
-        parcel.writeString(releaseDate);
+        parcel.writeLong(releaseDate);
+        parcel.writeDouble(popularity);
         parcel.writeDouble(voteAverage);
         parcel.writeString(overview);
+        parcel.writeInt(favorite ? 1 : 0);
     }
 }
