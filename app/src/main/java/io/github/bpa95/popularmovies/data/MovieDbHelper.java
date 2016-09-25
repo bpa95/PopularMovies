f66ff9d9463902db378837ae1b685dff92f9b9c8
bpa95 <berpaval@gmail.com>
2016-09-25 20:35:14 +0300

    feat: Create base intent service

--- app/src/main/java/io/github/bpa95/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/io/github/bpa95/popularmovies/data/MovieDbHelper.java
@@ -31,7 +31,6 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                 MovieEntry.COLUMN_VOTE_AVERAGE + " REAL NOT NULL,  " +
                 MovieEntry.COLUMN_OVERVIEW + " TEXT NOT NULL,  " +
 
-                // TODO don't ignore
                 " UNIQUE (" + MovieEntry.COLUMN_MOVIE_ID + ") ON CONFLICT IGNORE);";
 
         final String SQL_CREATE_TRAILER_TABLE = "CREATE TABLE " + TrailerEntry.TABLE_NAME + " (" +
