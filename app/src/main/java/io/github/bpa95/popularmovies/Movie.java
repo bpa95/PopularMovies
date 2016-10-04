5300e5e180598ff601950d45afee50f8fe9a555b
bpa95 <berpaval@gmail.com>
2016-10-04 23:00:02 +0300

    refactor: Move method related to movies in Movie class

--- app/src/main/java/io/github/bpa95/popularmovies/Movie.java
+++ app/src/main/java/io/github/bpa95/popularmovies/Movie.java
@@ -4,10 +4,19 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
@@ -15,6 +24,7 @@ import java.util.Locale;
 import io.github.bpa95.popularmovies.data.MoviesContract.MovieEntry;
 
 public class Movie implements Parcelable {
+    private static final String LOG_TAG = Movie.class.getSimpleName();
     public int id;
     public Uri posterPath;
     public String title;
@@ -55,7 +65,7 @@ public class Movie implements Parcelable {
                 .build();
     }
 
-    Movie(Cursor cursor) {
+    public Movie(Cursor cursor) {
         id = cursor.getInt(cursor.getColumnIndex(MovieEntry.COLUMN_MOVIE_ID));
         posterPath = Uri.parse(cursor.getString(cursor.getColumnIndex(MovieEntry.COLUMN_POSTER_PATH)));
         title = cursor.getString(cursor.getColumnIndex(MovieEntry.COLUMN_TITLE));
@@ -63,6 +73,94 @@ public class Movie implements Parcelable {
         popularity = cursor.getDouble(cursor.getColumnIndex(MovieEntry.COLUMN_POPULARITY));
         voteAverage = cursor.getDouble(cursor.getColumnIndex(MovieEntry.COLUMN_VOTE_AVERAGE));
         overview = cursor.getString(cursor.getColumnIndex(MovieEntry.COLUMN_OVERVIEW));
+    }
+
+    /**
+     * Extracts movie data from json string and returns array of movies with this data.
+     *
+     * @param moviesJsonStr json string to parse
+     * @return array of movies
+     * @throws JSONException if passed string is not appropriate json string
+     * @throws IOException   if passed string is null or empty
+     */
+    public static Movie[] getMovieDataFromJson(String moviesJsonStr) throws JSONException, IOException {
+        if (moviesJsonStr == null || moviesJsonStr.isEmpty()) {
+            throw new IOException("Empty json");
+        }
+
+        final String TMDB_RESULTS = "results";
+
+        JSONArray moviesJson = new JSONObject(moviesJsonStr)
+                .getJSONArray(TMDB_RESULTS);
+
+        int length = moviesJson.length();
+        Movie[] movies = new Movie[length];
+        for (int i = 0; i < length; i++) {
+            movies[i] = new Movie(moviesJson.getJSONObject(i));
+        }
+
+        return movies;
+    }
+
+    /**
+     * Constructs the URL for the Movie Database query, which contains the json object
+     * with movie data. Movies will be in order specified by parameter.
+     *
+     * @param sortOrder part of path which specify sort order
+     * @return correct url to The Movie Database from which the json object with movie data can be fetched
+     * @throws MalformedURLException should never happen
+     */
+    @NonNull
+    public static URL constructUrl(final String sortOrder) throws MalformedURLException {
+        final String MOVIES_BASE_URL = "http://api.themoviedb.org/3";
+        final String API_KEY_PARAM = "api_key";
+
+        Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                .appendEncodedPath(sortOrder)
+                .appendQueryParameter(API_KEY_PARAM, BuildConfig.MOVIE_DATABASE_API_KEY)
+                .build();
+
+        Log.v(LOG_TAG, builtUri.toString());
+
+        return new URL(builtUri.toString());
+    }
+
+    /**
+     * Reads bytes from given input stream into string. It is assumed that
+     * input stream contains correct json object.
+     *
+     * @param inputStream stream with correct json object
+     * @return json string read from given stream
+     * @throws IOException in case of errors in input stream
+     */
+    public static String getJsonString(InputStream inputStream) throws IOException {
+        if (inputStream == null) {
+            return null;
+        }
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            StringBuilder buffer = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line).append('\n');
+            }
+
+            return buffer.toString();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
     }
 
 
--- app/src/main/java/io/github/bpa95/popularmovies/sync/SyncAdapter.java
+++ app/src/main/java/io/github/bpa95/popularmovies/sync/SyncAdapter.java
@@ -11,30 +11,23 @@ import android.content.SharedPreferences;
 import android.content.SyncRequest;
 import android.content.SyncResult;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import io.github.bpa95.popularmovies.BuildConfig;
 import io.github.bpa95.popularmovies.MainActivity;
 import io.github.bpa95.popularmovies.Movie;
 import io.github.bpa95.popularmovies.R;
 import io.github.bpa95.popularmovies.data.MoviesContract;
+
+import static io.github.bpa95.popularmovies.Movie.getJsonString;
 
 /**
  * Handle the transfer of data between a server and an
@@ -64,14 +57,14 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         try {
             // Construct the URL for the Movie Database query
             String sortOrder = getContext().getString(R.string.pref_sortOrder_popular_value);
-            URL url = constructUrl(sortOrder);
+            URL url = Movie.constructUrl(sortOrder);
 
             // Create the request to TheMovieDatabase, and open the connection
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
-            Movie[] movies = getMovieDataFromJson(getJsonString(urlConnection.getInputStream()));
+            Movie[] movies = Movie.getMovieDataFromJson(getJsonString(urlConnection.getInputStream()));
 
             ContentValues[] cvArray = new ContentValues[movies.length];
             for (int i = 0; i < movies.length; i++) {
@@ -125,93 +118,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         }
     }
 
-    /**
-     * Reads bytes from given input stream into string. It is assumed that
-     * input stream contains correct json object.
-     *
-     * @param inputStream stream with correct json object
-     * @return json string read from given stream
-     * @throws IOException in case of errors in input stream
-     */
-    private String getJsonString(InputStream inputStream) throws IOException {
-        if (inputStream == null) {
-            return null;
-        }
-        BufferedReader reader = null;
 
-        try {
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            StringBuilder buffer = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line).append('\n');
-            }
-
-            return buffer.toString();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
-                }
-            }
-        }
-    }
-
-    /**
-     * Extracts movie data from json string and returns array of movies with this data.
-     *
-     * @param moviesJsonStr json string to parse
-     * @return array of movies
-     * @throws JSONException if passed string is not appropriate json string
-     * @throws IOException   if passed string is null or empty
-     */
-    private Movie[] getMovieDataFromJson(String moviesJsonStr) throws JSONException, IOException {
-        if (moviesJsonStr == null || moviesJsonStr.isEmpty()) {
-            throw new IOException("Empty json");
-        }
-
-        final String TMDB_RESULTS = "results";
-
-        JSONArray moviesJson = new JSONObject(moviesJsonStr)
-                .getJSONArray(TMDB_RESULTS);
-
-        int length = moviesJson.length();
-        Movie[] movies = new Movie[length];
-        for (int i = 0; i < length; i++) {
-            movies[i] = new Movie(moviesJson.getJSONObject(i));
-        }
-
-        return movies;
-    }
-
-    /**
-     * Constructs the URL for the Movie Database query, which contains the json object
-     * with movie data. Movies will be in order specified by parameter.
-     *
-     * @param sortOrder part of path which specify sort order
-     * @return correct url to The Movie Database from which the json object with movie data can be fetched
-     * @throws MalformedURLException should never happen
-     */
-    @NonNull
-    private URL constructUrl(final String sortOrder) throws MalformedURLException {
-        final String MOVIES_BASE_URL = "http://api.themoviedb.org/3";
-        final String API_KEY_PARAM = "api_key";
-
-        Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
-                .appendEncodedPath(sortOrder)
-                .appendQueryParameter(API_KEY_PARAM, BuildConfig.MOVIE_DATABASE_API_KEY)
-                .build();
-
-        Log.v(LOG_TAG, builtUri.toString());
-
-        return new URL(builtUri.toString());
-    }
 
     /**
      * Helper method to have the sync adapter sync immediately
